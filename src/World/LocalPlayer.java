60fa555529edeab42e762d72027b79d98fefb2f3
pimterry <devnull@localhost>
2010-01-08 02:35:48 +0000

    Sorted player controls so that absolute controlled players can go at 45 degrees, and can turn w/o moving.

--- src/World/LocalPlayer.java
+++ src/World/LocalPlayer.java
@@ -21,10 +21,18 @@ public class LocalPlayer extends Player {
   public static final byte RELATIVE_CONTROLS = 0;
   public static final byte ABSOLUTE_CONTROLS = 1;
 
-  private static final byte FORWARD_KEY = GameCanvas.UP_PRESSED;
-  private static final byte BACKWARD_KEY = GameCanvas.DOWN_PRESSED;
-  private static final byte LEFT_KEY = GameCanvas.LEFT_PRESSED;
-  private static final byte RIGHT_KEY = GameCanvas.RIGHT_PRESSED;
+  private static final byte UP = GameCanvas.UP_PRESSED;
+  private static final byte DOWN = GameCanvas.DOWN_PRESSED;
+  private static final byte LEFT = GameCanvas.LEFT_PRESSED;
+  private static final byte RIGHT = GameCanvas.RIGHT_PRESSED;
+  
+  private static final byte UP_RIGHT = UP | RIGHT;
+  private static final byte UP_LEFT = UP | LEFT;
+  private static final byte DOWN_RIGHT = DOWN | RIGHT;
+  private static final byte DOWN_LEFT = DOWN | LEFT;
+
+  private static final byte DIRECTION_MASK = UP | DOWN | LEFT | RIGHT;
+
   private static final int FIRE_KEY = GameCanvas.FIRE_PRESSED;
 
   private static byte controlType = ABSOLUTE_CONTROLS;
@@ -61,54 +69,68 @@ public class LocalPlayer extends Player {
     if (lastKeyState == 0) return;
 
     if (controlType == ABSOLUTE_CONTROLS)
-    { // TODO - don't move if turning?
-      if ((lastKeyState & FORWARD_KEY) != 0)
+    {
+      if ((lastKeyState & DIRECTION_MASK) != 0)
       {
-        planSetAngle(0);
+        switch((lastKeyState & DIRECTION_MASK))
+        {
+          case UP:
+            planSetAngle(0);
+            break;
+          case UP_RIGHT:
+            planSetAngle(2);
+            break;
+          case RIGHT:
+            planSetAngle(4);
+            break;
+          case DOWN_RIGHT:
+            planSetAngle(6);
+            break;
+          case DOWN:
+            planSetAngle(8);
+            break;
+          case DOWN_LEFT:
+            planSetAngle(10);
+            break;
+          case LEFT:
+            planSetAngle(12);
+            break;
+          case UP_LEFT:
+            planSetAngle(14);
+            break;
+        }
+
+        // If any direction key was pressed, move in the direction we're now facing.
         planMove(SPEED);
-      }
-      if ((lastKeyState & BACKWARD_KEY) != 0)
-      {
-        planSetAngle(8);
-        planMove(SPEED);
-      }
-      if ((lastKeyState & LEFT_KEY) != 0)
-      {
-        planSetAngle(12);
-        planMove(SPEED);
-      }
-      if ((lastKeyState & RIGHT_KEY) != 0)
-      {
-        planSetAngle(4);
-        planMove(SPEED);
-      }
-      if ((lastKeyState & FIRE_KEY) != 0)
-      {
-        planFire();
       }
     }
     else if (controlType == RELATIVE_CONTROLS)
     {
-      if ((lastKeyState & FORWARD_KEY) != 0)
+      if ((lastKeyState & UP) != 0)
       {
         planMove(SPEED);
       }
-      if ((lastKeyState & BACKWARD_KEY) != 0)
+      if ((lastKeyState & DOWN) != 0)
       {
         planMove( - SPEED);
       }
 
       // We enforce delay on turning moves because otherwise it's way too fast to be easily controllable.
-      if ((lastKeyState & LEFT_KEY) != 0 && System.currentTimeMillis() - lastTurnTime > TURNING_DELAY)
+      if ((lastKeyState & LEFT) != 0 && System.currentTimeMillis() - lastTurnTime > TURNING_DELAY)
       {
         lastTurnTime = System.currentTimeMillis();
         planTurn(-1);
       }
-      if ((lastKeyState & RIGHT_KEY) != 0 && System.currentTimeMillis() - lastTurnTime > TURNING_DELAY)
+      if ((lastKeyState & RIGHT) != 0 && System.currentTimeMillis() - lastTurnTime > TURNING_DELAY)
       {
         lastTurnTime = System.currentTimeMillis();
         planTurn(1);
       }
+    }
+
+    if ((lastKeyState & FIRE_KEY) != 0)
+    {
+      planFire();
     }
   }
 
